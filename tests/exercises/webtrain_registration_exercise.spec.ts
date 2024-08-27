f27b2811642cc6e0de1fb534445895ee8e259a60
michalcapoun <michal.capoun@gmail.com>
2024-08-27 14:46:52 +0200

    fix locator format

--- tests/exercises/webtrain_registration_exercise.spec.ts
+++ tests/exercises/webtrain_registration_exercise.spec.ts
@@ -7,11 +7,11 @@ test.skip("E2E Registration", async ({ page }) => {
   await page.locator("#email").fill("mc@mc.cz");
   await page.locator("#password").fill("123456");
   await page.locator("#confirm-password").fill("123456");
-  await page.locator('//input[@value="basic"]').check();
-  await page.locator('//input[@id="age"]').fill("33");
+  await page.locator("#basic").check();
+  await page.locator("#age").fill("33");
   await page.locator("#gender").selectOption("male");
   await page.locator("#address").fill("123456");
-  await page.locator('//input[@id="gender"]').selectOption("Sports");
-  await page.locator('//input[@id="newsletter"]').check();
+  await page.locator("#interests-music").check();
+  await page.locator("#newsletter").check();
   await page.locator('button[type="submit"]').click();
 });
